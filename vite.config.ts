24ed7bf0ec808ac1212bd477e06c3fb3aee4edda
Seungbeen Wi <seungbeenwi@Seungbeenui-MacBookPro.local>
2025-01-05 00:53:35 +0900

    CSS setting change

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 글로벌 SCSS 파일을 포함할 경우
+        additionalData: `@import './src/styles/global.scss';`
+      }
+    }
+  },
+
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
